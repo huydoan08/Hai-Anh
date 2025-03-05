b9eabffe68485584b013a9931d9444e0f055dcbd
huy.nguyen <huy.nguyen8@sotatek.com>
2025-03-05 23:50:32 +0700

    f

--- src/app/(demo)/dau-tay-1-den-2-tuoi/page.tsx
+++ src/app/(demo)/dau-tay-1-den-2-tuoi/page.tsx
@@ -24,7 +24,7 @@ export default function DauTayHaiTuoiPage() {
   };
 
   return (
-    <ContentLayout title="Đoàn Hải Anh (Dâu Tây).">
+    <ContentLayout title="Đoàn Hải Anh (Dâu Tây)">
       <Card className="max-h-[67.5vh] overflow-hidden shadow-lg border border-gray-200 dark:border-gray-700">
         <CardContent className="p-6 space-y-4">
           <div className="relative w-full h-[400px] overflow-hidden rounded-lg group bg-black/10 dark:bg-white/10">
