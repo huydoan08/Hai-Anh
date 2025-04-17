46359c3b450b2129817efdd0ac16cdcd427f4088
huy.doan <doanvanhuy268@gmail.com>
2025-04-17 15:02:11 +0700

    f

--- src/app/(demo)/raising-children/data.ts
+++ src/app/(demo)/raising-children/data.ts
@@ -1,5 +1,12 @@
 export const raisingchildren = [
   {
+    title:
+      "Bức thư của Tổng thống Abraham Lincoln gửi thầy giáo của con trai mình",
+    content: [
+      "Kính gửi thầy giáo,\n\nCon trai tôi sẽ phải học tất cả những điều này \n\nTôi biết rằng cậu bé sẽ không phải là một đứa trẻ mãi mãi. Và cậu ấy sẽ phải học rằng, không phải ai cũng công bằng, không phải ai cũng chân thật.\n\nXin thầy hãy dạy cho cháu biết rằng, cứ mỗi kẻ ác, lại có một người tốt.\nCứ mỗi chính trị gia ích kỷ, lại có một nhà lãnh đạo tận tụy.\n\nXin dạy cho cháu rằng, một đồng tiền kiếm được bằng mồ hôi sẽ đáng giá hơn nhiều so với năm đồng nhặt được.\n\nXin hãy giúp cháu tránh xa sự ganh tị và học cách tận hưởng niềm vui chiến thắng.\n\nXin hãy dạy cho cháu biết cách đối mặt với thất bại và làm thế nào để biết cúi đầu trong chiến thắng.\n\nXin hãy dạy cho cháu biết: người hay bắt nạt là kẻ yếu nhất, và người mạnh thực sự là người biết tha thứ.\n\nXin hãy dạy cho cháu biết rằng: giá trị của đồng tiền là điều cần thiết nhưng không phải là tất cả.\n\nXin hãy dạy cho cháu biết cách lắng nghe mọi người, nhưng cũng dạy cháu cách lọc ra sự thật từ những gì nghe được.\n\nXin hãy dạy cho cháu biết cách cười khi buồn, rằng không có gì xấu hổ khi khóc.\n\nXin hãy dạy cháu biết rằng: đôi khi im lặng lại là câu trả lời tốt nhất.\n\nXin hãy dạy cháu biết rằng: những người bạn thật sự là tài sản quý giá nhất.\n\nCuối cùng, xin hãy dạy cháu biết: sống một cuộc đời đầy lòng tin vào bản thân và luôn tin vào nhân loại.\n\nĐó là một yêu cầu lớn, nhưng xin thầy hãy cố gắng.\nCháu là một đứa trẻ tốt – con trai tôi."
+    ]
+  },
+  {
     title: "Bánh mì kẹp giáo dục",
     content: [
       "Không cho cái gì Quá đà, cho nhiều quá => CHÁN, cho vừa đủ, hơi thiếu => HÁO HỨC",
@@ -7,15 +14,6 @@ export const raisingchildren = [
       "Đối xử như bạn thân, đồng lõa, đồng hành",
       "Ân và UY + giải thích cho Hiểu => Nâng đỡ, Nghiêm khắc, Hỗ Trợ, Tuân thủ luật chơi, Tự do, Trách nhiệm",
       "Cùng con quan sát, phân tích, đúc kết từ những trải nghiệm trong cuộc sống"
-    ]
-  },
-  {
-    title: "Bức thư của Abraham Lincoln gửi thầy giáo con trai",
-    content: [
-      "Xin hãy dạy cho cháu biết phải lắng nghe tất cả mọi người nhưng cũng xin thầy dạy cho cháu biết cần phải sàng lọc những gì nghe được qua một tấm lưới chân lý để cháu chỉ đón nhận những gì tốt đẹp...",
-      "Xin hãy dạy cho cháu biết cách mỉm cười khi buồn bã, xin hãy dạy cháu biết rằng không có sự xấu hổ trong những giọt nước mắt",
-      "Xin hãy dạy cho cháu biết chế giễu những kẻ yếm thế và cẩn trọng trước sự ngọt ngào đầy cạm bẫy",
-      "Xin hãy dạy cho cháu rằng có thể bán cơ bắp và trí tuệ cho người ta giá cao nhất, nhưng không bao giờ cho phép ai ra giá mua trái tim và tâm hồn mình."
     ]
   },
   {
